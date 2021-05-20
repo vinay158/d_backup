0087b3598ad5a12f41447a58dac8094c9c3b978c
vinay158 <vinayverma158@gmail.com>
2021-05-19 17:41:28 -0700

    demov5 19 may 2021 v2

--- models/Sparkpost_mail_model.php
+++ models/Sparkpost_mail_model.php
@@ -1,8 +1,83 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require_once 'vendor/sparkpost_autoload.php';
+
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+
 class Sparkpost_mail_model extends CI_Model{	
 	
+	public function requestSparkPostApi($action, $requestData){
+		
+		
+		$sparkPostDetail = $this->query_model->getbySpecific('tbl_sparkpost_mail', 'id', 1);
+		
+		$request_result = array();
+		
+		if(!empty($sparkPostDetail)){
+			
+			if($sparkPostDetail[0]->type == 1 && !empty($sparkPostDetail[0]->api_key)){
+				
+				if(!empty($requestData)){
+					
+					if($action == "add_template"){
+						$request_result = $this->addTemplateToSparkpostApi($sparkPostDetail, $requestData);
+					}
+					
+				}
+				
+			}
+		}
+		
+		return $request_result;		
+		
+	}
+	
+	public function addTemplateToSparkpostApi($sparkPostDetail,$requestData){
+		
+		$httpClient = new GuzzleAdapter(new Client());
+		$sparky = new SparkPost($httpClient, ['key'=>$sparkPostDetail[0]->api_key]);
+		$sparky->setOptions(['async' => false]);
+		
+		$sparkpost_template_id = '';
+		$result = array();
+		$result['response'] = 0;
+		
+		$name = (isset($requestData['title']) && !empty($requestData['title'])) ? $requestData['title'] : '';
+		$subject = (isset($requestData['subject']) && !empty($requestData['subject'])) ? $requestData['subject'] : '';
+		$html = (isset($requestData['description']) && !empty($requestData['description'])) ? $requestData['description'] : '';
+		
+		$create_template_promise = $sparky->request('POST', 'templates', [
+				  'name' => $name,
+				  'published' => true,
+				  'content' => [
+					'from' => $sparkPostDetail[0]->from_email,
+					'subject' => $subject,
+					'html' => $html,
+				  ],
+				]);
 
+				try {
+					$create_template_response_code = $create_template_promise->getStatusCode();
+					$create_template = $create_template_promise->getBody();
+					
+					if($create_template_response_code == 200){
+						if(isset($create_template['results']['id']) && !empty($create_template['results']['id'])){
+							
+							$result['response'] = 1;
+							$result['template_id'] = $create_template['results']['id'];
+						}
+					}
+					
+				} catch (\Exception $e) {
+					echo $e->getCode()."\n";
+					echo $e->getMessage()."\n";
+				}
+		
+		return $result;
+		
+	}
 	
 	
 }
---
+++ views/admin/sparkpost_mail_index.php
@@ -0,0 +1,137 @@
+<?php $this->load->view("admin/include/header"); ?>
+
+<div class="az-content-body-left advanced_page custom_full_page form_module_listing_page sparkpost_mail_flow_page" >
+	<div class="az-content-header d-block d-md-flex">
+		<div class="az-dashboard-one-title">
+			<div>
+			  <h2 class="az-dashboard-title"><?=$title?></h2>
+			</div>
+			
+		  </div>
+	  </div>
+	   <div class="row row-sm program-cat-page">
+
+          <div class="col-sm-12 col-xl-12 az-content-body-contacts"  >
+			<div class="card pd-sm-20 edit-form">
+			
+<div class="gen-holder">
+
+	<div class="gen-panel-holder"  style="width: 100% !important;">
+	<!--<div class="mb-3 main-content-label page_main_heading">Virtual Training</div>
+
+<div class="">
+
+<!--<div class="btn-addentry add_entry_button"></div>-->
+
+<?php 
+
+	if(!empty($sparkpost_mail_flows)){ 
+		foreach($sparkpost_mail_flows as $sparkpost_flow){
+			
+?>
+<h1 > &nbsp;&nbsp; </h1>
+
+
+<div class="program_full_detail new_lisiting_block default_template" id="AlternatingFullWidth">
+
+				<div class="mb-3 main-content-label " style="margin-top:0px !important" ><?php echo $sparkpost_flow->title; ?>
+				
+				<span style="float:right"><a  class="duplicate_mail_flow_btn ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$sparkpost_flow->id;?>"   table_name="tbl_sparkpost_mail_flows" item_title="<?=$sparkpost_flow->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate Email Flow</a></span>
+				</div>
+				<div class="row row-xs align-items-center  mg-t-25 mg-b-5">
+					<div class="col-md-12">
+						<p>You may rearrange the order of the items below by dragging them above or below other item rows. Changes will automatically be saved.</p>
+					</div>
+					
+				</div>
+				
+				<div class="row row-xs align-items-center mg-b-20  alternating-row-section ">
+					
+					<div class="col-md-12 mg-t-5 mg-md-t-0 ">
+						<div class="az-content-body ">
+			
+							  <div class="az-mail-header">
+								<div>
+								  <h4 class="az-content-title mg-b-5"><?php echo $sparkpost_flow->title; ?></h4>
+								  <p>You have <span class="total_alternating_full_width_row_<?php echo $sparkpost_flow->id; ?>"><?php echo !empty($sparkpost_templates[$sparkpost_flow->id]) ? count($sparkpost_templates[$sparkpost_flow->id]) : 0; ?></span> Entries</p>
+								</div>
+								<div>
+								 <a href="admin/<?=$link_type;?>/add_template/<?php echo $sparkpost_flow->id; ?>" class="button_class btn btn-indigo ">Add New Template</a>
+								</div>
+							  </div>
+							  
+							  
+			<ul class="ajax_record_sortable alternating_full_width_row"  table_name="tbl_sparkpost_mail_templates" id="email_flow_<?php echo $sparkpost_flow->id; ?>">
+
+			
+			<?php
+				$deleteBtnLimit = 1;
+				/*if($form_type->id == 1){
+					$deleteBtnLimit = 3;
+				}elseif($form_type->id == 2){
+					$deleteBtnLimit = 4;
+				}elseif($form_type->id == 3){
+					$deleteBtnLimit = 2;
+				}elseif($form_type->id == 4){
+					$deleteBtnLimit = 3;
+				}*/
+				$i = 1;
+				foreach($sparkpost_templates[$sparkpost_flow->id] as $row):
+				
+				?>
+				
+
+					<li   id="menu_<?=$row->id?>" class="full_width_row_<?php echo $row->id; ?> az-contact-info-header">
+						<div class="manager-item media">
+							<div style="float:left;">
+								<div class="badge-no"><?=$i?>. </div>
+								
+									
+								<h4 class="full_width_row_heading_<?=$row->id?>"><a href="javascript:void(0)" ><?=$row->title;?> </a><em><?php //echo !empty($row->connected_type) ? ' - '.$row->connected_type : '';?></em></h4>
+								
+							</div>
+							<div class="manager-item-opts">
+							
+							
+						<nav class="nav">
+							
+							<?php if($user_level == 1) {?> 
+							  <a href="admin/<?=$link_type;?>/edit_template/<?php echo $row->id; ?>" class="badge badge-primary">Edit</a>
+							  
+							  <a  class="badge badge-primary ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$row->id;?>"   table_name="tbl_sparkpost_mail_templates" item_title="<?=$row->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate</a>
+							  
+							  <?php //if($i > $deleteBtnLimit){ ?>
+							 <a class="badge badge-primary ajax_record_delete" data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$row->id;?>"   table_name="tbl_sparkpost_mail_templates" item_title="<?=$row->title;?>" section_type="full_width_row_<?php echo $sparkpost_flow->id; ?>">Delete</a>
+							  <?php //} ?>
+							<?php } ?>
+							
+						</nav>
+
+
+
+							</div>
+						</div>
+					</li>
+<?php $i++; endforeach;?>	
+								</ul>
+
+	
+			
+						
+						
+				</div>
+			</div>
+		</div>
+</div>
+<?php } } ?>
+</div>	
+	<input type="hidden" name="mod_type" value="<?=$link_type;?>" id="mod_type" />	
+	</div>			
+
+		
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php $this->load->view("admin/include/footer");?>
