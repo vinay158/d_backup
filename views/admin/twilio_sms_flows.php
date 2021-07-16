18d82cd50a1b5317baca81d383f77e00a5f24f15
vinay158 <vinayverma158@gmail.com>
2021-07-15 17:57:58 -0700

    demov5 15 july 2021 v1

--- views/admin/twilio_sms_flows.php
+++ views/admin/twilio_sms_flows.php
@@ -25,8 +25,8 @@
 
 <?php 
 
-	if(!empty($sparkpost_mail_flows)){ 
-		foreach($sparkpost_mail_flows as $sparkpost_flow){
+	if(!empty($twilio_sms_flows)){ 
+		foreach($twilio_sms_flows as $twilio_sms_flow){
 			
 ?>
 <h1 > &nbsp;&nbsp; </h1>
@@ -35,14 +35,32 @@
 <div class="program_full_detail new_lisiting_block default_template" id="AlternatingFullWidth">
 
 				<div class="mb-3 main-content-label " style="margin-top:0px !important" >
-				<span class="flow_title_<?php echo $sparkpost_flow->id; ?>"><?php echo $sparkpost_flow->title; ?></span>
-				<input type="text" value="<?php echo $sparkpost_flow->title; ?>" class="flow_title_edit_input flow_title_input_<?php echo $sparkpost_flow->id; ?>"  flow_id="<?php echo $sparkpost_flow->id; ?>" style="display:none">
-				<a href="javascript:void(0)" class="flow_title_edit" flow_id="<?php echo $sparkpost_flow->id; ?>"><i class="fas fa-edit "></i></a>
+				<span class="flow_title_<?php echo $twilio_sms_flow->id; ?>"><?php echo $twilio_sms_flow->title; ?></span>
+				<input type="text" value="<?php echo $twilio_sms_flow->title; ?>" class="flow_title_edit_input flow_title_input_<?php echo $twilio_sms_flow->id; ?>"  flow_id="<?php echo $twilio_sms_flow->id; ?>" style="display:none">
+				<a href="javascript:void(0)" class="flow_title_edit" flow_id="<?php echo $twilio_sms_flow->id; ?>"><i class="fas fa-edit "></i></a>
 				
-				<span style="float:right"><a  class="duplicate_mail_flow_btn ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$sparkpost_flow->id;?>"   table_name="twilio_sms_flows" item_title="<?=$sparkpost_flow->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate SMS Flow</a>
+				<span style="float:right">
 				
-				<?php if($sparkpost_flow->id != 1){ ?>
-					<a id="delitem_<?=$sparkpost_flow->id?>" class="duplicate_mail_flow_btn ajax_record_delete" title='Delete <?=$sparkpost_flow->title;?>' data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$sparkpost_flow->id;?>"   table_name="twilio_sms_flows" item_title="<?=$sparkpost_flow->title;?>" section_type="little_row"><i class="fa fa-trash" ></i> Delete</a>
+				<?php if(!empty($pages_list)){ ?>
+					<select class="field page_list_dropdown flow_page_url"  flow_id="<?php echo $twilio_sms_flow->id; ?>">
+						
+						<?php if($twilio_sms_flow->id == 1){ ?>
+							<option value="all_forms"  <?php echo ($twilio_sms_flow->page_url == "all_forms") ? 'selected=selected' : ''; ?>>All Forms</option>
+						<?php }else{ ?>
+							<option value="">--Select Page--</option>
+							<?php foreach($pages_list as $key => $pages){ ?>
+								<option value=""><?php echo isset($form_types_list[$key]) ? $form_types_list[$key] : ''; ?></option>
+								<?php foreach($pages as $url => $page_name){ ?>
+									<option value="<?php echo $url; ?>" <?php echo ($twilio_sms_flow->page_url == $url) ? 'selected=selected' : ''; ?>><?php echo $page_name; ?></option>
+								<?php } ?>
+							<?php } ?>
+						<?php } ?>
+					</select>
+				<?php } ?>
+				<a  class="duplicate_mail_flow_btn ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$twilio_sms_flow->id;?>"   table_name="twilio_sms_flows" item_title="<?=$twilio_sms_flow->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate SMS Flow</a>
+				
+				<?php if($twilio_sms_flow->id != 1){ ?>
+					<a id="delitem_<?=$twilio_sms_flow->id?>" class="duplicate_mail_flow_btn ajax_record_delete" title='Delete <?=$twilio_sms_flow->title;?>' data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$twilio_sms_flow->id;?>"   table_name="twilio_sms_flows" item_title="<?=$twilio_sms_flow->title;?>" section_type="little_row"><i class="fa fa-trash" ></i> Delete</a>
 				<?php } ?>
 				</span>
 				
@@ -62,23 +80,23 @@
 			
 							  <div class="az-mail-header">
 								<div>
-								  <h4 class="az-content-title mg-b-5 flow_title_<?php echo $sparkpost_flow->id; ?>">
-								 <?php echo $sparkpost_flow->title; ?></h4>
-								  <p>You have <span class="total_alternating_full_width_row_<?php echo $sparkpost_flow->id; ?>"><?php echo !empty($sparkpost_templates['days_template'][$sparkpost_flow->id]) ? count($sparkpost_templates['days_template'][$sparkpost_flow->id]) : 0; ?></span> Entries</p>
+								  <h4 class="az-content-title mg-b-5 flow_title_<?php echo $twilio_sms_flow->id; ?>">
+								 <?php echo $twilio_sms_flow->title; ?></h4>
+								  <p>You have <span class="total_alternating_full_width_row_<?php echo $twilio_sms_flow->id; ?>"><?php echo !empty($twilio_sms_templates['days_template'][$twilio_sms_flow->id]) ? count($twilio_sms_templates['days_template'][$twilio_sms_flow->id]) : 0; ?></span> Entries</p>
 								</div>
 								<div>
-								 <a href="admin/<?=$link_type;?>/add_template/<?php echo $sparkpost_flow->id; ?>" class="button_class btn btn-indigo ">Add New Template</a>
+								 <a href="admin/<?=$link_type;?>/add_template/<?php echo $twilio_sms_flow->id; ?>" class="button_class btn btn-indigo ">Add New Template</a>
 								</div>
 							  </div>
 							  
 			<!-- ajax_record_sortable  class for sort templates--->				  
-			<ul class=" alternating_full_width_row"  table_name="tbl_twilio_sms_templates" id="sms_flow_<?php echo $sparkpost_flow->id; ?>">
+			<ul class=" alternating_full_width_row"  table_name="tbl_twilio_sms_templates" id="sms_flow_<?php echo $twilio_sms_flow->id; ?>">
 
 			
 			<?php
 				$deleteBtnLimit = 1;
 				$i = 1;
-				foreach($sparkpost_templates['days_template'][$sparkpost_flow->id] as $row):
+				foreach($twilio_sms_templates['days_template'][$twilio_sms_flow->id] as $row):
 				
 				?>
 				
@@ -89,7 +107,18 @@
 								<div class="badge-no"><?=$i?>. </div>
 								
 									
-								<h4 class="full_width_row_heading_<?=$row->id?>"><a href="javascript:void(0)" ><?=$row->title;?> </a><em><?php echo isset($template_types[$row->template_type]) ? ' - '.$template_types[$row->template_type] : '';?></em></h4>
+								<h4 class="full_width_row_heading_<?=$row->id?>"><a href="javascript:void(0)" ><?=$row->title;?> </a><em><?php echo isset($template_types[$row->template_type]) ? ' - '.$template_types[$row->template_type] : '';?></em>
+								
+								<em>
+									<?php 
+										if($row->template_type == "day_1"){
+											echo '(Sent immediately)';
+										}else{
+											echo !empty($row->send_sms_time) ? '('.date('h:i A', strtotime($row->send_sms_time)).')' : '';
+										}
+									?>
+								</em>
+								</h4>
 								
 								
 							</div>
@@ -105,7 +134,7 @@
 							  <a  class="badge badge-primary ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate</a>
 							  
 							  <?php //if($i > $deleteBtnLimit){ ?>
-							 <a class="badge badge-primary ajax_record_delete" data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width_row_<?php echo $sparkpost_flow->id; ?>">Delete</a>
+							 <a class="badge badge-primary ajax_record_delete" data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width_row_<?php echo $twilio_sms_flow->id; ?>">Delete</a>
 							  <?php //} ?>
 							<?php } ?>
 							
@@ -120,7 +149,7 @@
 								</ul>
 				</div>
 			
-			<?php if(isset($sparkpost_templates['other_templates'][$sparkpost_flow->id]) && !empty($sparkpost_templates['other_templates'][$sparkpost_flow->id])){?>	
+			<?php if(isset($twilio_sms_templates['other_templates'][$twilio_sms_flow->id]) && !empty($twilio_sms_templates['other_templates'][$twilio_sms_flow->id])){?>	
 				<div class="az-content-body ">
 					<div class="az-mail-header">
 								<div style="margin-top:50px">
@@ -130,13 +159,13 @@
 							  </div>
 							  
 			<!-- ajax_record_sortable  class for sort templates--->				  
-			<ul class=" alternating_full_width_row"  table_name="tbl_twilio_sms_templates" id="email_flow_paid_trial<?php echo $sparkpost_flow->id; ?>">
+			<ul class=" alternating_full_width_row"  table_name="tbl_twilio_sms_templates" id="email_flow_paid_trial<?php echo $twilio_sms_flow->id; ?>">
 
 			
 			<?php
 				$deleteBtnLimit = 1;
 				$i = 1;
-				foreach($sparkpost_templates['other_templates'][$sparkpost_flow->id] as $row):
+				foreach($twilio_sms_templates['other_templates'][$twilio_sms_flow->id] as $row):
 				
 				?>
 				
@@ -161,7 +190,7 @@
 							<!--  <a  class="badge badge-primary ajax_record_duplicate" data-toggle="modal" data-target="#popupDuplicateItem" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width" form_action="admin/<?=$link_type?>/duplicate_form" redirect_path="admin/<?=$link_type?>">Duplicate</a>
 							  
 							  <?php //if($i > $deleteBtnLimit){ ?>
-							 <a class="badge badge-primary ajax_record_delete" data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width_row_<?php echo $sparkpost_flow->id; ?>">Delete</a>
+							 <a class="badge badge-primary ajax_record_delete" data-toggle="modal" data-target="#popupDeleteRecord" item_id="<?=$row->id;?>"   table_name="tbl_twilio_sms_templates" item_title="<?=$row->title;?>" section_type="full_width_row_<?php echo $twilio_sms_flow->id; ?>">Delete</a>
 							  <?php //} ?>-->
 							<?php } ?>
 							
@@ -209,7 +238,7 @@
 			
 			$.ajax({ 					
 			type: 'POST',						
-			url: "admin/sparkpost_mail/ajax_update_flow_title",						
+			url: "admin/twilio_smsflow/ajax_update_flow_title",						
 			data: { flow_id : flow_id,title:new_title,'action':'update_title'}					
 			}).done(function(msg){ 
 			if(msg == 1){
@@ -223,6 +252,30 @@
 		});
 		})
 		
+		
+		$('body').on('change','.flow_page_url',function(){
+			var flow_id = $(this).attr('flow_id');
+			var page_url = $(this).val();
+			if(page_url == "" || page_url == null){
+				alert('Please select page for sms flow'); return false
+			}else{
+				$.ajax({ 					
+					type: 'POST',						
+					url: "admin/twilio_smsflow/ajax_update_flow_page_url",						
+					data: { flow_id : flow_id,page_url:page_url,'action':'update_page_url'}					
+					}).done(function(msg){ 
+					if(msg == 1){
+						
+					}
+					else{
+						alert("Oops! Something went wrong!");
+						return false;
+								
+					}
+				});
+			}
+			
+		})
 		
 		/*$( ".flow_title_edit_input" ).mouseout(function() {
 			$.each($('.flow_title_edit'),function(){
